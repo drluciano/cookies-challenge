8910a692cc634d3f8de9d4e673c5ff3017768bad
drluciano <dylanrivera@Dylans-MacBook-Pro.local>
2026-03-11 11:51:35 -0400

    Added login cookie @ /login/[username]

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/login/:username", function (req, res) {
+  const username = req.params.username;
+
+  var opts = {
+    maxAge: 90000,
+    httpOnly: true,
+  };
+
+  if (username) {
+    res.send(`Welcome, ${username}!`);
+  } else {
+    res.send("Please go to /login/[Username]");
+  }
+
+  res.cookie("username", username, opts);
+
+  app;
+});
+
+app.listen(port, () => console.log(`Now listening on port ${port}.`));
